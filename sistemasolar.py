1be706003c8c0482dc7fc47f7772fbc931daa663
lugima9 <luisgilmartin@correo.ugr.es>
2022-03-23 16:43:54 +0100

    git commit sistemasolar.py

--- sistemasolar.py
+++ sistemasolar.py
@@ -14,6 +14,7 @@
 #Nota: Instalar ffmpeg extensión para convertir vídeo
 
 import math as m
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -29,38 +30,89 @@ class Planetas:
 
 # Constantes globales
 
-global masaSol = 1.98847e30 # [kg]
-global G = 6.67430e-11 # [m3 kg-1 s-2]
+masaSol = 1.98847e30 # [kg]
+G = 6.67430e-11 # [m3 kg-1 s-2]
 
-# Lectura de datos de planetas
+# AYUDA PARA VISUALIZACIÓN
+# --------------
+# Array de posiciones instantáneas:
+# r = [[x_0, y_0], ..., [x_7, y_7]]
+# Si quiero la posición del primer planeta: posicion = r[0]
+# --------------
+# Array de velocidades instantáneas:
+# (ANÁLOGO)
+# --------------
 
-# Asignación de condiciones iniciales
+# Función que calcula la aceleración (x,y) de un planeta en un instante por acción del Sol y el resto de planetas
+# Recibe: índice del planeta, array de posiciones de cada planeta en (x,y), vector de masas de cada planeta
+# Devuelve: array 2D con aceleración (x,y) del planeta
+def calcAcelPlaneta(estePlaneta, posiciones, masas):
 
-# Algoritmo de Verlet
+    # Posición (x,y) con respecto al Sol del planeta y distancia cubo
+    difPos = posiciones[estePlaneta]
 
-def Verlet(h, t, r, v):
-    a
+    distCubo = np.linalg.norm(difPos)**3
 
-    t = t+h
-    for i in range(0, 7, 1)
-    r[i][0] = 
+    # Aceleración (x,y) del planeta debido al Sol
+    acel = -difPos/distCubo
 
+    # Aceleración (x,y) del planeta debido al resto de planetas
+    for i in range(0, 7, 1):
+        if i != estePlaneta:
+            difPos = posiciones[estePlaneta] - posiciones[i]      #Posición relativa entre planetas
+            distCubo = np.linalg.norm(difPos)**3
+            acel += masas[i]*difPos/distCubo                        #Sumamos la aceleración por el resto de planetas a la del Sol
 
-    def VerletPendulo(g, t_0, h, ang_0, vAng_0):
-    #Valores iniciales de las listas
-    t, ang, vAng, aAng, w = []*101, []*101, []*101, []*101, []*101
+    return acel
 
-    t.insert(0, t_0)
-    ang.insert(0, ang_0)
-    vAng.insert(0, vAng_0)
-    aAng.insert(0, -g*math.sin(ang_0))
-    for i in range(0, 100, 1):
-        t.insert(i+1, t[i] + h)
-        w.insert(i, vAng[i] + 0.5*h*aAng[i])
-        ang.insert(i+1, ang[i] + h*w[i])
-        aAng.insert(i+1, -g*math.sin(ang[i+1]))
-        vAng.insert(i+1, w[i] + 0.5*h*aAng[i+1])
+# Función que calcula un paso del algoritmo de Verlet para obtener la posición y velocidad de cada planeta en el tiempo
+# Recibe: paso, instante, 
+# Devuelve: lista con nuevos (instante, array de posiciones, array de velocidades)
 
-    return (t, ang, vAng)
+def pasoVerlet(h, t, r, v):
 
+    #Creo dos arrays 3D auxiliares de 8 (nº planetas) x 2 (x,y)
+    acel, w = np.zeros(8, 2), np.zeros(8, 2)
 
+    # Para cada uno de los 8 planetas...
+    for i in range(0, 7, 1):
+        #Calcula la aceleración (x,y) de cada planeta       
+        acel[i] = calcAcelPlaneta(i, r, m) #OJO: se mete el array r entero
+        
+        #Cálculo intermedio (x,y) de cada planeta
+        w[i] = v[i] + 0.5*h*acel[i]
+
+        #Calcula la nueva posición (x,y) de cada planeta
+        r[i] = r[i] + h*w[i]
+
+        #Calcula la nueva aceleración (x,y) de cada planeta
+        acel[i] = calcAcelPlaneta(i, r, m)
+
+        #Calcula la nueva velocidad (x,y) de cada planeta
+        v[i] = w[i] + 0.5*h*acel[i]
+    
+    t += h
+
+    return (t, r, v)
+
+# Función que obtiene los datos de los planetas de un fichero de texto
+# Recibe:  nombre del fichero
+# Devuelve:
+
+def leerCondIni(nombreFichero):
+     # Abrimos el fichero en modo lectura
+     f = open(nombreFichero, "r")
+
+     datos = f.read()
+
+     f.close()
+
+     return datos
+
+# FUNCIÓN PRINCIPAL
+
+#
+
+t=0
+h = 0.01
+t_max = 100
