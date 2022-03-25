b234d4127b75ebecfa4329ecd9a4ba1f4d0b0fc0
lugima9 <luisgilmartin@correo.ugr.es>
2022-03-25 16:50:32 +0100

    git commit sistemasolar.py

--- sistemasolar.py
+++ sistemasolar.py
@@ -8,32 +8,22 @@
 # Universidad de Granada
 #//////////////////////////////////////////////////////////////
 
-#Nota: Para el cálculo del periodo no podemos comprobar que pase dos veces por exactamente el mismo punto.
-#Podemos comprobar que el vector posición cambia de cuadrante con desigualdades u otra cosa que se nos ocurra.
-
-#Nota: Instalar ffmpeg extensión para convertir vídeo
-
 import math as m
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-#Clase de planetas (PROBAR CON LOS **ARGS)
+#-------------------------------------CLASES--------------------------------------------
 class Planetas:
-    def __init__(self, nombre, masa, distMedia, vel):
+    def __init__(self, nombre, masa, distMedia, vel, *args):
         self.nombre = nombre
         self.masa = masa
         self.distMedia = distMedia
         self.vel = vel
 
-# AYUDA PARA VISUALIZACIÓN
-# --------------
-# Array de posiciones instantáneas:
-# r = [[x_0, y_0], ..., [x_7, y_7]]
-# Si quiero la posición del primer planeta: posicion = r[0]
-# --------------
-# Array de velocidades instantáneas:
-# (ANÁLOGO)
-# --------------
+        self.args = args
+
+#------------------------------------FUNCIONES------------------------------------------
 
 # Función que calcula la aceleración (x,y) de un planeta en un instante por acción del Sol y el resto de planetas
 # Recibe: índice del planeta, array de posiciones de cada planeta en (x,y), vector de masas de cada planeta
@@ -57,11 +47,11 @@ def calcAcelPlaneta(estePlaneta, posiciones, masas):
 
     return acel
 
-# Función que calcula un paso del algoritmo de Verlet para obtener la posición y velocidad de cada planeta en el tiempo
-# Recibe: paso, instante, posiciones, velocidades
-# Devuelve: nuevos instante, array de posiciones, array de velocidades
 
-def pasoVerlet(h, t, r, v):
+# Función que calcula un paso del algoritmo de Verlet para obtener la posición y velocidad de cada planeta en el tiempo
+# Recibe: paso, masas, posiciones, velocidades
+# Devuelve: array de posiciones, array de velocidades
+def pasoVerlet(h, m, r, v):
 
     #Creo dos arrays 3D auxiliares de 8 (nº planetas) x 2 (x,y)
     acel, w = np.zeros(8, 2), np.zeros(8, 2)
@@ -82,51 +72,133 @@ def pasoVerlet(h, t, r, v):
 
         #Calcula la nueva velocidad (x,y) de cada planeta
         v[i] = w[i] + 0.5*h*acel[i]
-    
-    t += h
 
-    return t, r, v
+    return r, v
+
 
 # Función que obtiene los datos de los planetas de un fichero de texto
-# Recibe:  nombre del fichero con una fila por planeta
-# Devuelve: lista con string de datos para cada planeta 
-
-def leerCondIni(nombreFichero):
+# Recibe:  paso, nombre del fichero con una fila por planeta, bool de alineación
+# Devuelve: lista de planetas con sus atributos actualizados
+def condIni(paso, nombreFichero, alinear):
     # Abrimos el fichero en modo lectura
     f = open(nombreFichero, "r")
 
     # Leemos el archivo saltando el header y separamos en lista por planeta
     next(f)
     datos = f.read().split('\n')
-
-    # Asignamos a cada planeta sus atributos
-    p = []
-    for i in range(len(datos)):
-        d = datos[i].split()
-        p[i] = Planetas(d[0], d[1], d[2], d[3])
-
     f.close()
 
-    return p
+    # Guardamos cada dato de cada planeta en un elemento distinto de cada array
+    nombres, masas, distancias, velocidades = [], [], [], []
+    for i in range(len(datos)):
+        d = datos[i].split()
+        nombres[i] = d[0]
+        masas[i] = d[1]
+        distancias[i] = d[2]
+        velocidades[i] = d[3]
+    
+    # Reescalamos los datos
+    paso, masas, distancias, velocidades = reescala(paso, masas, distancias, velocidades)
+
+    # Iniciamos posiciones y velocidades
+
+    if alinear == True:
+        r, v = inicioAlineado(distancias, velocidades)
+    else:
+        r, v = inicioAleatorio(distancias, velocidades)
+
+    return paso, nombres, masas, r, v
+
 
 # Función que realiza el cambio de unidades de reescalado
-# Recibe: paso, array de posiciones iniciales, array de masas
-# Devuelve: lista con paso, array de posiciones iniciales, array de masas (reescalado)
-def reescala(h, r, m):
-    c = 299792458           #[m s-1]
+# Recibe: paso, array de masas, array de posiciones iniciales y array de velocidades iniciales
+# Devuelve: paso, array de masas, array de posiciones iniciales y array de velocidades iniciales (reescalado)
+def reescala(paso, masas, distancias, velocidades):
+
+    c = 299792458           # [m s-1]
     masaSol = 1.98847e30    # [kg]
     G = 6.67430e-11         # [m3 kg-1 s-2]
 
-    h = sqrt(G*masaSol/c**3)*h
-    r = r/c
-    m = m/masaSol
+    paso = paso*(G*masaSol/c**3)**(0.5)
+    masas = masas/masaSol
+    distancias = distancias/c
+    velocidades = velocidades*(c/(G*masaSol))**(0.5)
 
-    return (h, r, m)
+    return paso, masas, distancias, velocidades
 
-# FUNCIÓN PRINCIPAL
+# Función que aleatoriza el inicio de la simulación
+# Recibe: distancias y velocidades iniciales
+# Devuelve: array de posiciones y array de velocidades con formato (x,y) para cada elemento-planeta
+def inicioAleatorio(distancias, velocidades):
 
-#
+    # Para cada planeta (tantos como elementos en distancias[])
+    r, v = np.zeros(8, 2), np.zeros(8, 2)
+    for i in range(len(distancias)):
 
+        # Genero una coordenada polar aleatoria
+        angulo = random.randrange(0., 2.*np.pi)
+        coseno = m.cos(angulo)
+        seno = m.sin(angulo)
+
+        # Calculo una posición inicial en cartesianas
+        r[i,1] = distancias[i]*coseno
+        r[i,2] = distancias[i]*seno
+
+        # Calculo una velocidad inicial perpendicular al vector r en cartesianas
+        v[i,1] = -velocidades[i]*seno
+        v[i,2] = velocidades[i]*coseno        
+
+    return r, v
+
+
+# Función que alinea ápsides planetas al inicio de la simulación
+# Recibe: distancias y velocidades iniciales
+# Devuelve: array de posiciones y array de velocidades con formato (x,y) para cada elemento-planeta
+def inicioAlineado(distancias, velocidades):
+
+    # Para cada planeta (tantos como elementos en distancias[])
+    r, v = np.zeros(8, 2), np.zeros(8, 2)
+    for i in range(len(distancias)):
+
+        # Fijo posición inicial en (x,0)
+        r[i,1] = distancias[i]
+        r[i,2] = 0
+
+        # Fijo velocidad inicial en (0, v)
+        v[i,1] = 0
+        v[i,2] = velocidades[i]       
+
+    return r, v
+
+#------------------------------------FUNCIÓN PRINCIPAL------------------------------------------
+
+# Leemos las condiciones iniciales y las guardamos en los correspondientes arrays (ya reescaladas)
+h = float(input("Introduzca el paso: \n"))
+
+alinear = ""
+while alinear != "Y" and alinear != "N":
+    alinear = bool("¿Desea que los planetas comiencen alineados? (Y/N): \n")
+
+nombreArchivo = "datos_planetas.txt"
+h, nombres, masas, r, v = condIni(h, nombreArchivo, alinear)
+
+# Simulamos el Sistema Solar durante un tiempo fijado
 t=0
-h = 0.01
 t_max = 100
+
+while t < t_max:
+
+    r, v = pasoVerlet(h, m, r, v)
+
+    # DEBO IR ESCRIBIENDO LOS DATOS EN UN FICHERO EN EL FORMATO QUE INDICA EL SCRIPT DE JUANI
+
+    t += h
+
+
+
+
+
+
+
+
+
